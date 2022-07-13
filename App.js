e6089a34bea5dabcdd33dab01c6b9bb50069fdab
Cristian Gustavo <cristian.dias@terraviva.agr.br>
2022-07-13 18:03:46 -0300

    feat: Estilização tela inicial projeto, arquivo 'App.js'

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from "react";
 
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, StyleSheet } from 'react-native'
 
 import Home from './src/telas/Home'
 
 function App() {
-  return <SafeAreaView>
+  return <SafeAreaView style={estilos.container}>
     <Home></Home>
   </SafeAreaView>
 }
 
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+})
+
 export default App
+
